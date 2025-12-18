338c0798f38e4cd4f7b2c3768b6da35f4369cabd
Joan Piedra <joan@joanpiedra.com>
2025-12-18 15:34:05 -0500

    usar metodos void en listado y metodos de formato de Principal

--- venta_microprocesadores/src/final_project/Listar.java
+++ venta_microprocesadores/src/final_project/Listar.java
@@ -85,110 +85,137 @@ public class Listar extends JDialog implements ActionListener {
 		imprimir("LISTADO DE MICROPROCESADORES");
 		imprimir("");
 		
+		// imprimir modelos
+		imprimirMod1();
+		imprimirMod2();
+		imprimirMod3();
+		imprimirMod4();
+		imprimirMod5();
+		imprimirMod6();
+		imprimirMod7();
+		imprimirMod8();
+		imprimirMod9();
+		imprimirMod10();
+
+		// mostrar la primera linea del textarea
+		scrollTop();
+	}
+
+	private void imprimirMod1() {
 		imprimir("Nombre \t\t\t\t : " + Principal.modelo1);
-		imprimir("Precio \t\t\t\t : $" + String.format("%.2f", Principal.precio1));
+		imprimir("Precio \t\t\t\t : " + Principal.formatPrecio(Principal.precio1));
 		imprimir("Fabricante \t\t\t : " + Principal.fabricante1);
 		imprimir("Número de Núcleos \t\t : " + Principal.nucleos1);
 		imprimir("Número de Hilos \t\t : " + Principal.hilos1);
-		imprimir("Velocidad de Núcleos \t\t : " + Principal.velocidad1 + " Ghz");
-		if (Principal.video1 == '1') imprimir("Tarjeta de Video Integrada \t : Incluye");
-		else imprimir("Tarjeta de Video Integrada \t : No Incluye");
+		imprimir("Velocidad de Núcleos \t\t : " + Principal.formatGhz(Principal.velocidad1));
+		imprimir("Tarjeta de Video Integrada \t : " + Principal.formatVideo(Principal.video1));
 		imprimir("\n");
-		
+	}
+
+	private void imprimirMod2() {
 		imprimir("Nombre \t\t\t\t : " + Principal.modelo2);
-		imprimir("Precio \t\t\t\t : $" + String.format("%.2f", Principal.precio2));
+		imprimir("Precio \t\t\t\t : " + Principal.formatPrecio(Principal.precio2));
 		imprimir("Fabricante \t\t\t : " + Principal.fabricante2);
 		imprimir("Número de Núcleos \t\t : " + Principal.nucleos2);
 		imprimir("Número de Hilos \t\t : " + Principal.hilos2);
-		imprimir("Velocidad de Núcleos \t\t : " + Principal.velocidad2 + " Ghz");
-		if (Principal.video2 == '1') imprimir("Tarjeta de Video Integrada \t : Incluye");
-		else imprimir("Tarjeta de Video Integrada \t : No Incluye");
+		imprimir("Velocidad de Núcleos \t\t : " + Principal.formatGhz(Principal.velocidad2));
+		imprimir("Tarjeta de Video Integrada \t : " + Principal.formatVideo(Principal.video2));
 		imprimir("\n");
+	}
 
+	private void imprimirMod3() {
 		imprimir("Nombre \t\t\t\t : " + Principal.modelo3);
-		imprimir("Precio \t\t\t\t : $" + String.format("%.2f", Principal.precio3));
+		imprimir("Precio \t\t\t\t : " + Principal.formatPrecio(Principal.precio3));
 		imprimir("Fabricante \t\t\t : " + Principal.fabricante3);
 		imprimir("Número de Núcleos \t\t : " + Principal.nucleos3);
 		imprimir("Número de Hilos \t\t : " + Principal.hilos3);
-		imprimir("Velocidad de Núcleos \t\t : " + Principal.velocidad3 + " Ghz");
-		if (Principal.video3 == '1') imprimir("Tarjeta de Video Integrada \t : Incluye");
-		else imprimir("Tarjeta de Video Integrada \t : No Incluye");
+		imprimir("Velocidad de Núcleos \t\t : " + Principal.formatGhz(Principal.velocidad3));
+		imprimir("Tarjeta de Video Integrada \t : " + Principal.formatVideo(Principal.video3));
 		imprimir("\n");
+	}
 
+	private void imprimirMod4() {
 		imprimir("Nombre \t\t\t\t : " + Principal.modelo4);
-		imprimir("Precio \t\t\t\t : $" + String.format("%.2f", Principal.precio4));
+		imprimir("Precio \t\t\t\t : " + Principal.formatPrecio(Principal.precio4));
 		imprimir("Fabricante \t\t\t : " + Principal.fabricante4);
 		imprimir("Número de Núcleos \t\t : " + Principal.nucleos4);
 		imprimir("Número de Hilos \t\t : " + Principal.hilos4);
-		imprimir("Velocidad de Núcleos \t\t : " + Principal.velocidad4 + " Ghz");
-		if (Principal.video4 == '1') imprimir("Tarjeta de Video Integrada \t : Incluye");
-		else imprimir("Tarjeta de Video Integrada \t : No Incluye");
+		imprimir("Velocidad de Núcleos \t\t : " + Principal.formatGhz(Principal.velocidad4));
+		imprimir("Tarjeta de Video Integrada \t : " + Principal.formatVideo(Principal.video4));
 		imprimir("\n");
-		
+	}
+
+	private void imprimirMod5() {
 		imprimir("Nombre \t\t\t\t : " + Principal.modelo5);
-		imprimir("Precio \t\t\t\t : $" + String.format("%.2f", Principal.precio5));
+		imprimir("Precio \t\t\t\t : " + Principal.formatPrecio(Principal.precio5));
 		imprimir("Fabricante \t\t\t : " + Principal.fabricante5);
 		imprimir("Número de Núcleos \t\t : " + Principal.nucleos5);
 		imprimir("Número de Hilos \t\t : " + Principal.hilos5);
-		imprimir("Velocidad de Núcleos \t\t : " + Principal.velocidad5 + " Ghz");
-		if (Principal.video5 == '1') imprimir("Tarjeta de Video Integrada \t : Incluye");
-		else imprimir("Tarjeta de Video Integrada \t : No Incluye");
+		imprimir("Velocidad de Núcleos \t\t : " + Principal.formatGhz(Principal.velocidad5));
+		imprimir("Tarjeta de Video Integrada \t : " + Principal.formatVideo(Principal.video5));
 		imprimir("\n");
-		
+	}
+
+	private void imprimirMod6() {
 		imprimir("Nombre \t\t\t\t : " + Principal.modelo6);
-		imprimir("Precio \t\t\t\t : $" + String.format("%.2f", Principal.precio6));
+		imprimir("Precio \t\t\t\t : " + Principal.formatPrecio(Principal.precio6));
 		imprimir("Fabricante \t\t\t : " + Principal.fabricante6);
 		imprimir("Número de Núcleos \t\t : " + Principal.nucleos6);
 		imprimir("Número de Hilos \t\t : " + Principal.hilos6);
-		imprimir("Velocidad de Núcleos \t\t : " + Principal.velocidad6 + " Ghz");
-		if (Principal.video6 == '1') imprimir("Tarjeta de Video Integrada \t : Incluye");
-		else imprimir("Tarjeta de Video Integrada \t : No Incluye");
+		imprimir("Velocidad de Núcleos \t\t : " + Principal.formatGhz(Principal.velocidad6));
+		imprimir("Tarjeta de Video Integrada \t : " + Principal.formatVideo(Principal.video6));
 		imprimir("\n");
-		
+	}
+
+	private void imprimirMod7() {
 		imprimir("Nombre \t\t\t\t : " + Principal.modelo7);
-		imprimir("Precio \t\t\t\t : $" + String.format("%.2f", Principal.precio7));
+		imprimir("Precio \t\t\t\t : " + Principal.formatPrecio(Principal.precio7));
 		imprimir("Fabricante \t\t\t : " + Principal.fabricante7);
 		imprimir("Número de Núcleos \t\t : " + Principal.nucleos7);
 		imprimir("Número de Hilos \t\t : " + Principal.hilos7);
-		imprimir("Velocidad de Núcleos \t\t : " + Principal.velocidad7 + " Ghz");
-		if (Principal.video7 == '1') imprimir("Tarjeta de Video Integrada \t : Incluye");
-		else imprimir("Tarjeta de Video Integrada \t : No Incluye");
+		imprimir("Velocidad de Núcleos \t\t : " + Principal.formatGhz(Principal.velocidad7));
+		imprimir("Tarjeta de Video Integrada \t : " + Principal.formatVideo(Principal.video7));
 		imprimir("\n");
-		
+	}
+
+	private void imprimirMod8() {
 		imprimir("Nombre \t\t\t\t : " + Principal.modelo8);
-		imprimir("Precio \t\t\t\t : $" + String.format("%.2f", Principal.precio8));
+		imprimir("Precio \t\t\t\t : " + Principal.formatPrecio(Principal.precio8));
 		imprimir("Fabricante \t\t\t : " + Principal.fabricante8);
 		imprimir("Número de Núcleos \t\t : " + Principal.nucleos8);
 		imprimir("Número de Hilos \t\t : " + Principal.hilos8);
-		imprimir("Velocidad de Núcleos \t\t : " + Principal.velocidad8 + " Ghz");
-		if (Principal.video8 == '1') imprimir("Tarjeta de Video Integrada \t : Incluye");
-		else imprimir("Tarjeta de Video Integrada \t : No Incluye");
+		imprimir("Velocidad de Núcleos \t\t : " + Principal.formatGhz(Principal.velocidad8));
+		imprimir("Tarjeta de Video Integrada \t : " + Principal.formatVideo(Principal.video8));
 		imprimir("\n");
-		
+	}
+
+	private void imprimirMod9() {
 		imprimir("Nombre \t\t\t\t : " + Principal.modelo9);
-		imprimir("Precio \t\t\t\t : $" + String.format("%.2f", Principal.precio9));
+		imprimir("Precio \t\t\t\t : " + Principal.formatPrecio(Principal.precio9));
 		imprimir("Fabricante \t\t\t : " + Principal.fabricante9);
 		imprimir("Número de Núcleos \t\t : " + Principal.nucleos9);
 		imprimir("Número de Hilos \t\t : " + Principal.hilos9);
-		imprimir("Velocidad de Núcleos \t\t : " + Principal.velocidad9 + " Ghz");
-		if (Principal.video9 == '1') imprimir("Tarjeta de Video Integrada \t : Incluye");
-		else imprimir("Tarjeta de Video Integrada \t : No Incluye");
+		imprimir("Velocidad de Núcleos \t\t : " + Principal.formatGhz(Principal.velocidad9));
+		imprimir("Tarjeta de Video Integrada \t : " + Principal.formatVideo(Principal.video9));
 		imprimir("\n");
-		
+	}
+
+	private void imprimirMod10() {
 		imprimir("Nombre \t\t\t\t : " + Principal.modelo10);
-		imprimir("Precio \t\t\t\t : $" + String.format("%.2f", Principal.precio10));
+		imprimir("Precio \t\t\t\t : " + Principal.formatPrecio(Principal.precio10));
 		imprimir("Fabricante \t\t\t : " + Principal.fabricante10);
 		imprimir("Número de Núcleos \t\t : " + Principal.nucleos10);
 		imprimir("Número de Hilos \t\t : " + Principal.hilos10);
-		imprimir("Velocidad de Núcleos \t\t : " + Principal.velocidad10 + " Ghz");
-		if (Principal.video10 == '1') imprimir("Tarjeta de Video Integrada \t : Incluye");
-		else imprimir("Tarjeta de Video Integrada \t : No Incluye");
-
-		// hacer scroll a la primera linea
-		txtS.setCaretPosition(0);
+		imprimir("Velocidad de Núcleos \t\t : " + Principal.formatGhz(Principal.velocidad10));
+		imprimir("Tarjeta de Video Integrada \t : " + Principal.formatVideo(Principal.video10));
 	}
 	
 	void imprimir(String cad) {
 		txtS.append(cad + "\n");
+	}
+
+	// hacer scroll a la primera linea
+	void scrollTop() {
+		txtS.setCaretPosition(0);
 	}
 }
