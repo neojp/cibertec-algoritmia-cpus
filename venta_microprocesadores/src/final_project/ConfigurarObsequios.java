faf0fe56ccb4eba1c0c95d6ddc31720dd1d738f2
Joan Piedra <joan@joanpiedra.com>
2025-12-18 11:26:22 -0500

    actualizar obsequios en base al enunciado

--- venta_microprocesadores/src/final_project/ConfigurarObsequios.java
+++ venta_microprocesadores/src/final_project/ConfigurarObsequios.java
@@ -1,6 +1,7 @@
 package final_project;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.FlowLayout;
 
 import javax.swing.JButton;
@@ -8,6 +9,7 @@ import javax.swing.JDialog;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
@@ -44,54 +46,53 @@ public class ConfigurarObsequios extends JDialog implements ActionListener {
 		setTitle("Configurar Obsequios");
 		setBounds(100, 100, 415, 134);
 		getContentPane().setLayout(null);
-		{
-			lblObs1 = new JLabel("1 Unidad");
-			lblObs1.setBounds(10, 11, 100, 14);
-			getContentPane().add(lblObs1);
-		}
-		{
-			txtObs1 = new JTextField("" + Principal.obs1);
-			lblObs1.setLabelFor(txtObs1);
-			txtObs1.setBounds(140, 8, 129, 20);
-			getContentPane().add(txtObs1);
-			txtObs1.setColumns(10);
-		}
-		{
-			lblObs2 = new JLabel("2 a 5 unidades");
-			lblObs2.setBounds(10, 39, 100, 14);
-			getContentPane().add(lblObs2);
-		}
-		{
-			txtObs2 = new JTextField("" + Principal.obs2);
-			lblObs2.setLabelFor(txtObs2);
-			txtObs2.setColumns(10);
-			txtObs2.setBounds(140, 36, 129, 20);
-			getContentPane().add(txtObs2);
-		}
-		{
-			lblObs3 = new JLabel("6 a más unidades");
-			lblObs3.setBounds(10, 67, 100, 14);
-			getContentPane().add(lblObs3);
-		}
-		{
-			txtObs3 = new JTextField("" + Principal.obs3);
-			lblObs3.setLabelFor(txtObs3);
-			txtObs3.setColumns(10);
-			txtObs3.setBounds(140, 64, 129, 20);
-			getContentPane().add(txtObs3);
-		}
-		{
-			btnAceptar = new JButton("Aceptar");
-			btnAceptar.addActionListener(this);
-			btnAceptar.setBounds(300, 7, 89, 23);
-			getContentPane().add(btnAceptar);
-		}
-		{
-			btnCancelar = new JButton("Cancelar");
-			btnCancelar.addActionListener(this);
-			btnCancelar.setBounds(300, 35, 89, 23);
-			getContentPane().add(btnCancelar);
-		}
+
+		lblObs1 = new JLabel("1 Unidad");
+		lblObs1.setBounds(10, 11, 100, 14);
+		getContentPane().add(lblObs1);
+
+		txtObs1 = new JTextField();
+		lblObs1.setLabelFor(txtObs1);
+		txtObs1.setBounds(140, 8, 129, 20);
+		getContentPane().add(txtObs1);
+		txtObs1.setColumns(10);
+
+		lblObs2 = new JLabel("2 a 5 unidades");
+		lblObs2.setBounds(10, 39, 100, 14);
+		getContentPane().add(lblObs2);
+
+		txtObs2 = new JTextField();
+		lblObs2.setLabelFor(txtObs2);
+		txtObs2.setColumns(10);
+		txtObs2.setBounds(140, 36, 129, 20);
+		getContentPane().add(txtObs2);
+
+		lblObs3 = new JLabel("6 a más unidades");
+		lblObs3.setBounds(10, 67, 100, 14);
+		getContentPane().add(lblObs3);
+
+		txtObs3 = new JTextField();
+		lblObs3.setLabelFor(txtObs3);
+		txtObs3.setColumns(10);
+		txtObs3.setBounds(140, 64, 129, 20);
+		getContentPane().add(txtObs3);
+
+		btnAceptar = new JButton("Aceptar");
+		btnAceptar.addActionListener(this);
+		btnAceptar.setBackground(new Color(0, 64, 128));
+		btnAceptar.setForeground(new Color(255, 255, 255));
+		btnAceptar.setBounds(300, 7, 89, 23);
+		getContentPane().add(btnAceptar);
+
+		btnCancelar = new JButton("Cancelar");
+		btnCancelar.addActionListener(this);
+		btnCancelar.setBackground(new Color(128, 64, 64));
+		btnCancelar.setForeground(new Color(255, 255, 255));
+		btnCancelar.setBounds(300, 35, 89, 23);
+		getContentPane().add(btnCancelar);
+		
+		// actualizar campos con variables globales
+		actualizarForm();
 	}
 
 	public void actionPerformed(ActionEvent e) {
@@ -103,15 +104,47 @@ public class ConfigurarObsequios extends JDialog implements ActionListener {
 		}
 	}
 	protected void actionPerformedBtnAceptar(ActionEvent e) {
-		// actualizar obsequio1, obsequio2, obsequio3
-		String obsequio1 = txtObs1.getText();
-		String obsequio2 = txtObs2.getText();
-		String obsequio3 = txtObs3.getText();
-		Principal.obs1 = obsequio1;
-		Principal.obs2 = obsequio2;
-		Principal.obs3 = obsequio3;
+		// validar formulario
+		if (!validarFormulario()) {
+			return;
+		}
+
+		// actualizar obsequios
+		actualizarObs();
+		
+		// cerrar ventana
 		dispose();
 	}
+	
+	// validar si los campos txtObs1, txtObs2, txtObs3 estan vacíos
+	private boolean validarFormulario() {
+		try {
+			if (txtObs1.getText().trim().isEmpty() || txtObs2.getText().trim().isEmpty() || txtObs3.getText().trim().isEmpty()) {
+				throw new Exception("Campo no puede estar vacío");
+			}
+		} catch(Exception e) {
+			JOptionPane.showMessageDialog(this, e.getMessage(), "Error de validación", JOptionPane.ERROR_MESSAGE);
+			return false;
+		}
+		
+		return true;
+	}
+
+	// actualizar campos con variables globales
+	private void actualizarForm() {
+		txtObs1.setText("" + Principal.obsequio1);
+		txtObs2.setText("" + Principal.obsequio2);
+		txtObs3.setText("" + Principal.obsequio3);
+	}
+
+	// actualizar variables globales obsequio1, obsequio2, obsequio3
+	private void actualizarObs() {
+		Principal.obsequio1 = txtObs1.getText();
+		Principal.obsequio2 = txtObs2.getText();
+		Principal.obsequio3 = txtObs3.getText();
+	}
+	
+	// cerrar ventana
 	protected void actionPerformedBtnCancelar(ActionEvent e) {
 		dispose();
 	}
--- venta_microprocesadores/src/final_project/Principal.java
+++ venta_microprocesadores/src/final_project/Principal.java
@@ -138,12 +138,12 @@ public class Principal extends JFrame implements ActionListener {
 	public static double porcentaje1 = 7.5;  // Descuento para 1 a 5 unidades
 	public static double porcentaje2 = 10.0; // Descuento para 6 a 10 unidades
 	public static double porcentaje3 = 12.5; // Descuento para 11 a 15 unidades
-	public static double porcentaje4 = 15.0; // Descuento para más de 15 unidades
+	public static double porcentaje4 = 15.0; // Descuento para mï¿½s de 15 unidades
 
 	// Configuracion: Obsequios (Joan)
-	public static String obs1 = "Lapicero";
-	public static String obs2 = "Llavero";
-	public static String obs3 = "Memoria USB";
+	public static String obsequio1 = "Lapicero";    // Obsequio para 1 unidad
+	public static String obsequio2 = "Llavero";     // Obsequio para 2 a 5 unidades
+	public static String obsequio3 = "Memoria USB"; // Obsequio para 6 a mÃ¡s unidades
 	
 	/**
 	 * Launch the application.
